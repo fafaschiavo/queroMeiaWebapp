f84bf9dd39cebba932e5ed97a6aa954a1a6ad5d1
fafaschiavo <fayschiavo@gmail.com>
2016-04-19 01:04:19 -0300

    Interface Development

--- queroMeiaWebapp/settings.py
+++ queroMeiaWebapp/settings.py
@@ -93,6 +93,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'quero_meia',                      # Or path to database file if using sqlite3.
+#         'USER': 'fafaschiavo',                      # Not used with sqlite3.
+#         'PASSWORD': '310308Fah!',                  # Not used with sqlite3.
+#         'HOST': 'mysql.queromeia.com',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
